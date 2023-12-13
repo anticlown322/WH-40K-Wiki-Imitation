34ac520f57b6d76c71e3c78dc688149b3dc1908c
anticlown322 <karasandrey2005@gmail.com>
2023-12-14 00:04:15 +0300

    feat: initial commit
    create files for project, made sort of structure

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,49 +1,10 @@
 import "./styles/App.css";
-import {useState} from "react";
-import PostList from "./components/PostList.jsx";
-import MyButton from "./components/UI/button/MyButton.jsx"
-import MyInput from "./components/UI/input/MyInput.jsx";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascrscrscr 1', body:'тело пьяное'},
-        {id: 2, title: 'Javascrscrscr 2', body:'тело пьяное'},
-        {id: 3, title: 'Javascrscrscr 3', body:'тело пьяное'},
-        {id: 4, title: 'Javascrscrscr 4', body:'тело пьяное'},
-    ])
-
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-
-    const addNewPost = (e) => {
-        e.preventDefault()
-        const newPost = {
-            id: Date.now(),
-            title,
-            body
-        }
-        setPosts([...posts, newPost])
-    }
 
     return (
         <div className="App">
-            <form>
-                <MyInput
-                    value={title}
-                    onChange={e=>setTitle(e.target.value)}
-                    type="text"
-                    placeholder="Название поста"
-                />
-                <MyInput
-                    value={body}
-                    onChange={e=>setBody(e.target.value)}
-                    type="text"
-                    placeholder="Описание поста"
-                />
 
-                <MyButton onClick={addNewPost}>создать</MyButton>
-            </form>
-            <PostList posts={posts} title="Список джавкаскрскрскр"/>
         </div>
     )
 }
--- project/src/components/UI/myInput/MyInput.jsx
+++ project/src/components/UI/myInput/MyInput.jsx
@@ -3,7 +3,6 @@ import classes from "./MyInput.module.css";
 const MyInput = (props) => {
     return (
         <input className={classes.myInput} {...props}/>
-            
     );
 };
 
