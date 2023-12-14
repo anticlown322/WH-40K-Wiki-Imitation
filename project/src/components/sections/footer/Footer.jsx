7bf1f380c3519993f896d652680dc6fbd65dcaae
anticlown322 <karasandrey2005@gmail.com>
2023-12-15 01:41:04 +0300

    feat: add footer

--- project/src/components/sections/footer/Footer.jsx
+++ project/src/components/sections/footer/Footer.jsx
@@ -1,8 +1,31 @@
-const Footer = () => {
-    return (
-        <div>
+import { Box, Container, Grid, Typography } from "@mui/material";
 
-        </div>
+export const Footer = ()=> {
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: "auto",
+                backgroundColor: "gray",
+                paddingTop: "2rem",
+                paddingBottom: "1rem"
+            }}
+        >
+            <Container maxWidth="lg">
+                <Grid container direction="column" alignItems="center">
+                    <Grid item xs={12}>
+                        <Typography color="black" variant="h5">
+                            React Starter App
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography color="textSecondary" variant="subtitle1">
+                            {`${new Date().getFullYear()} | React | Material UI | React Router | addfsdsf | fsdgadfsdf | trtdgagrds`}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     );
 };
 
