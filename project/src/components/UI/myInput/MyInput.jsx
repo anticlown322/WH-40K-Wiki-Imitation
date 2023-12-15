eebe262f7b569b1c43dc4f3d9b86923afd99d9ed
anticlown322 <karasandrey2005@gmail.com>
2023-12-15 18:54:47 +0300

    refactor: delete nonusable components

--- project/src/components/UI/myInput/MyInput.jsx
+++
@@ -1,9 +0,0 @@
-import classes from "./MyInput.module.css";
-
-const MyInput = (props) => {
-    return (
-        <input className={classes.myInput} {...props}/>
-    );
-};
-
-export default MyInput;
