e51d5b3218c2b959b65509c39f6344ee627215d0
anticlown322 <karasandrey2005@gmail.com>
2023-12-19 16:55:52 +0300

    feat: add char search + refactor: char of the day

---
+++ project/src/components/UI/characterSearch/CharacterSearch.jsx
@@ -0,0 +1,35 @@
+import {useState} from 'react';
+import data from "../../../assets/docs/all-characters.json";
+import {TextField, Typography, Box} from "@mui/material";
+import Container from "@mui/material/Container";
+import classes from "./CharactersSearch.module.css";
+
+const CharacterSearch = () => {
+    let charactersArr;
+    const [value, setValue] = useState('')
+
+    return (
+        <Container maxWidth="xl">
+            <Box className={classes.outerContainer}>
+                <Typography variant="h3" color="white" fontWeight="bold">Поиск по имени</Typography>
+                <TextField
+                    label="Standard warning"
+                    variant="standard"
+                    color="warning"
+                    value={value}
+                    onChange={event => {
+                        setValue(event.target.value);
+                        charactersArr = []
+                        data.characters.forEach(obj => {
+                            if (obj.name.toLowerCase().includes(value.toLowerCase())) charactersArr.push(obj);
+                        })
+                        console.log(charactersArr);
+                    }
+                    }
+                />
+            </Box>
+        </Container>
+    );
+};
+
+export default CharacterSearch;
