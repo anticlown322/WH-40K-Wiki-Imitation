f232b6f2ab4056b2e2534acc44dc3a591d6c3647
anticlown322 <karasandrey2005@gmail.com>
2023-12-20 03:51:29 +0300

    refactor

--- project/src/components/UI/charactersGrid/CharactersGrid.jsx
+++ project/src/components/UI/charactersGrid/CharactersGrid.jsx
@@ -8,33 +8,34 @@ import CardMedia from '@mui/material/CardMedia';
 
 const CharactersGrid = (charArray) => {
     return (
-        <Container maxWidth="lg" className={classes.outerContainer}>
-            <Typography variant="h1" fontWeight="bold" color="white" align="center">Персонажи</Typography>
+        <Container maxWidth="xl" className={classes.outerContainer}>
+            <Typography variant="h1" fontWeight="bold" color="white" align="center" marginBottom={3}>Персонажи</Typography>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 {charArray.map((value) => (
                     <Grid item key={value} xs={12} sm={6} md={4}>
                         <Card
-                            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                            className={classes.card}
                         >
                             <CardMedia
                                 component="div"
                                 sx={{
                                     // 16:9
-                                    pt: '56.25%',
+                                    //pt: '56.25%',
+                                    height: '500px'
                                 }}
                                 image={value.mainImg}
                             />
                             <CardContent sx={{ flexGrow: 1 }}>
-                                <Typography gutterBottom variant="h5" component="h2">
+                                <Typography gutterBottom variant="h5" component="h2" color="white">
                                     {value.name}
                                 </Typography>
-                                <Typography>
+                                <Typography color="white">
                                     {value.shortDesc}
                                 </Typography>
                             </CardContent>
                             <CardActions>
                                 <Button
-                                    variant="contained" href={value.id} size="small" sx={{backgroundColor:'red'}} >
+                                    variant="contained" href={value.id} size="md" sx={{backgroundColor:'red'}} >
                                     Перейти на страницу
                                 </Button>
                             </CardActions>
